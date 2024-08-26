44ea00c8bb2baaf9a88bea3b1ac77becd42d7fba
Ahmed Abdelhafiez <nevoexists@gmail.com>
2024-08-26 12:45:14 +0300

    initial commit

---
+++ src/js/script.js
@@ -0,0 +1,26 @@
+const containerEl = document.querySelector(".container");
+
+const careers = ["YouTuber", "Web Developer", "Freelancer", "Instructor"];
+
+let careerIndex = 0;
+let charIndex = 0;
+
+const updateText = function () {
+  charIndex++;
+  containerEl.innerHTML = `<h1>I am ${careers[careerIndex].slice(0,1) === "I" ? "an" : "a"} ${careers[careerIndex].slice(
+    0,
+    charIndex
+  )}</h1>`;
+
+  if(charIndex === careers[careerIndex].length){
+    careerIndex++
+    charIndex = 0;
+  }
+
+  if(careerIndex === careers.length){
+    careerIndex = 0;
+  }
+  setTimeout(updateText, 400);
+};
+
+updateText();
